41625b7efd20bdf1fe3df519f3f303e62d4e1550
oferraro <oscar@conan-ui.com>
2020-04-27 21:04:25 -0300

    Use Guzzle for limit, set and get metadata

--- app/Http/Controllers/MercadoPagoController.php
+++ app/Http/Controllers/MercadoPagoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use \MercadoPago;
+use Illuminate\Support\Facades\Http;
 
 class MercadoPagoController extends Controller
 {
@@ -33,7 +34,7 @@ class MercadoPagoController extends Controller
 
     public function pay(Request $request) {
         $mpAccessToken = env("MERCADO_PAGO_ENV_ACCESS");
-         MercadoPago\SDK::setAccessToken($mpAccessToken);               // Set MercadoPago keys
+        MercadoPago\SDK::setAccessToken($mpAccessToken);               // Set MercadoPago keys
         $formData = $request->get('formData');
         // Use the payment Token
         $paymentToken = $request->get('MercadopagoPaymentId');
@@ -46,6 +47,9 @@ class MercadoPagoController extends Controller
         $cardAttributesPayMethod = $cardAttributes['payment_method'];
         // Use the datat to create the payment
         $payment = new MercadoPago\Payment();
+        $payment->metadata = [
+            "order_id" => time(),
+        ];
         $payment->transaction_amount = $formData['cartAmount'];         // $formData['cartAmount'];
         $payment->token = $paymentToken;
         $payment->description = "Ergonomic Paper Plate"; // TODO: Add the proper description here
@@ -53,7 +57,6 @@ class MercadoPagoController extends Controller
         $payment->payment_method_id = $cardAttributesPayMethod->id;
         $payment->issuer_id = $cardIssuerAttributes->id; // card issuer
         $payment->payer = [
-            "order_id" => time(),
             "email" => $customer->email // TODO: use more customer info here? add orderID?
         ];
         $payment->save();
@@ -70,13 +73,43 @@ class MercadoPagoController extends Controller
         $mpAccessToken = env("MERCADO_PAGO_ENV_ACCESS");
         MercadoPago\SDK::setAccessToken($mpAccessToken);
 
-        $payments = MercadoPago\SDK::get(
-        "/v1/payments/search"
-        );
+        /*  MercadoPago api doesn't work (use Guzzle)
+            $payments = MercadoPago\SDK::get(
+            "/v1/payments/search",
+                [ // TODO: define pagination if required
+                    "status" => "approved",
+                    "offset" => "0",
+                    "limit" => "3",
+                    "sort" => "id",
+                    "criteria" => "desc"
+                ]
+            );
+        */
+
+        $params = [
+            // TODO: define pagination if required
+            "status" => "approved",
+            "offset" => "0",
+            "limit" => "3",
+            "sort" => "id",
+            "criteria" => "desc",
+        ];
+        $getParams = '';
+        foreach ($params as $k => $v) {
+            $getParams.="&$k=$v";
+        }
+        $url = "https://api.mercadopago.com/v1/payments/search?access_token=$mpAccessToken&$getParams";
+        error_log(print_r($url, 1), 3, '/tmp/log');
+        $response = Http::get($url)->json();
 
         return response()->json([
-            'full_response' => $payments,
-            'payments' => $payments['body']['results'],
+            'payments' => $response,
         ]);
     }
 }
+
+/*
+curl -G -X GET -H "accept: application/json" "https://api.mercadopago.com/v1/payments/search" \
+-d "access_token=TEST-7577306854336871-052219-d510527538af0fee546eb6e583f08fcb__LA_LB__-185098070" \
+-d "status=approved" -d "offset=0" -d "limit=3" -d "sort=id" -d "criteria=desc"
+*/
